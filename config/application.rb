acfea8473d067eefc6550fbcbce4135369593cb2
andre pires <andrecabralpires@gmail.com>
2017-02-28 10:35:46 +0000

    newsbot commit

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,9 @@ module Newsbot
     config.generators do |generate|
       generate.assets false
     end
-
+config.paths.add File.join("app","bot"), glob:
+File.join("**","*rb")
+config.autoload_paths += Dir[Rails.root.join("app","bot","*")]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
